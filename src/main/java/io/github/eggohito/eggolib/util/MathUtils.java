fe58a1db147c5bf85170c311d17428844480c60d
eggohito <66972816+eggohito@users.noreply.github.com>
2022-04-12 16:57:27 +0800

    Tweak `MathUtil.java` Make the `operate()` method return a `Pair<Integer, Integer>`

--- src/main/java/io/github/eggohito/eggolib/util/MathUtils.java
+++ src/main/java/io/github/eggohito/eggolib/util/MathUtils.java
@@ -4,6 +4,7 @@ import io.github.apace100.apoli.power.CooldownPower;
 import io.github.apace100.apoli.power.Power;
 import io.github.apace100.apoli.power.VariableIntPower;
 import net.minecraft.scoreboard.ScoreboardPlayerScore;
+import net.minecraft.util.Pair;
 
 public class MathUtils {
 
@@ -15,89 +16,71 @@ public class MathUtils {
         MODULO,
         MIN,
         MAX,
-        SWAP;
+        SWAP
+    }
+
+    private static Pair<Integer, Integer> result;
+
+    public static Pair<Integer, Integer> operate(int a, Operation operation, int b) {
+        switch (operation) {
+            case ADD:
+                a = a + b;
+                break;
+            case SUBTRACT:
+                a = a - b;
+                break;
+            case MULTIPLY:
+                a = a * b;
+                break;
+            case DIVIDE:
+                a = Math.floorDiv(a, b);
+                break;
+            case MODULO:
+                a = Math.floorMod(a, b);
+                break;
+            case MIN:
+                a = Math.min(a, b);
+                break;
+            case MAX:
+                a = Math.max(a, b);
+                break;
+            case SWAP:
+                int a2 = a;
+                a = b;
+                b = a2;
+                break;
+        }
+        return new Pair<>(a, b);
     }
 
     public static void operateScores(ScoreboardPlayerScore targetScore, Operation operation, ScoreboardPlayerScore sourceScore) {
 
-        int targetValue = targetScore.getScore();
-        int sourceValue = sourceScore.getScore();
+        int x = targetScore.getScore();
+        int y = sourceScore.getScore();
 
-        switch (operation) {
-            case ADD:
-                targetValue = targetValue + sourceValue;
-                break;
-            case SUBTRACT:
-                targetValue = targetValue - sourceValue;
-                break;
-            case MULTIPLY:
-                targetValue = targetValue * sourceValue;
-                break;
-            case DIVIDE:
-                targetValue = Math.floorDiv(targetValue, sourceValue);
-                break;
-            case MODULO:
-                targetValue = Math.floorMod(targetValue, sourceValue);
-                break;
-            case MIN:
-                targetValue = Math.min(targetValue, sourceValue);
-                break;
-            case MAX:
-                targetValue = Math.max(targetValue, sourceValue);
-            case SWAP:
-                int a = targetValue;
-                targetValue = sourceValue;
-                sourceValue = a;
-                break;
-        }
+        result = operate(x, operation, y);
 
-        targetScore.setScore(targetValue);
-        sourceScore.setScore(sourceValue);
+        targetScore.setScore(result.getLeft());
+        sourceScore.setScore(result.getRight());
     }
 
     public static void operatePowers(Power targetPower, Operation operation, Power sourcePower) {
 
-        int targetValue = 0;
-        int sourceValue = 0 ;
+        int x = 0;
+        int y = 0 ;
 
-        if (targetPower instanceof VariableIntPower targetResource) targetValue = targetResource.getValue();
-        else if (targetPower instanceof CooldownPower targetCooldown) targetValue = targetCooldown.getRemainingTicks();
+        if (targetPower instanceof VariableIntPower targetResource) x = targetResource.getValue();
+        else if (targetPower instanceof CooldownPower targetCooldown) x = targetCooldown.getRemainingTicks();
 
-        if (sourcePower instanceof VariableIntPower sourceResource) sourceValue = sourceResource.getValue();
-        else if (sourcePower instanceof CooldownPower sourceCooldown) sourceValue = sourceCooldown.getRemainingTicks();
+        if (sourcePower instanceof VariableIntPower sourceResource) y = sourceResource.getValue();
+        else if (sourcePower instanceof CooldownPower sourceCooldown) y = sourceCooldown.getRemainingTicks();
 
-        switch (operation) {
-            case ADD:
-                targetValue = targetValue + sourceValue;
-                break;
-            case SUBTRACT:
-                targetValue = targetValue - sourceValue;
-                break;
-            case MULTIPLY:
-                targetValue = targetValue * sourceValue;
-                break;
-            case DIVIDE:
-                targetValue = Math.floorDiv(targetValue, sourceValue);
-                break;
-            case MODULO:
-                targetValue = Math.floorMod(targetValue, sourceValue);
-                break;
-            case MIN:
-                targetValue = Math.min(targetValue, sourceValue);
-                break;
-            case MAX:
-                targetValue = Math.max(targetValue, sourceValue);
-            case SWAP:
-                int a = targetValue;
-                targetValue = sourceValue;
-                sourceValue = a;
-                break;
-        }
+        result = operate(x, operation, y);
 
-        if (targetPower instanceof VariableIntPower targetResource) targetResource.setValue(targetValue);
-        else if (targetPower instanceof CooldownPower targetCooldown) targetCooldown.setCooldown(targetValue);
+        if (targetPower instanceof VariableIntPower targetResource) targetResource.setValue(result.getLeft());
+        else if (targetPower instanceof CooldownPower targetCooldown) targetCooldown.setCooldown(result.getLeft());
 
-        if (sourcePower instanceof VariableIntPower sourceResource) sourceResource.setValue(sourceValue);
-        else if (sourcePower instanceof CooldownPower sourceCooldown) sourceCooldown.setCooldown(sourceValue);
+        if (sourcePower instanceof VariableIntPower sourceResource) sourceResource.setValue(result.getRight());
+        else if (sourcePower instanceof CooldownPower sourceCooldown) sourceCooldown.setCooldown(result.getRight());
     }
 }
