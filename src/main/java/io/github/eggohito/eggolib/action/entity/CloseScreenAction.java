cc6175d255e8a30a1029c2f41340063e363e0f06
eggohito <66972816+eggohito@users.noreply.github.com>
2022-06-16 23:21:22 +0800

    Tweak `close_screen` and `set_perspective` entity action types Ensure that they work on the client and the server. This might not be needed, but just in case

--- src/main/java/io/github/eggohito/eggolib/action/entity/CloseScreenAction.java
+++ src/main/java/io/github/eggohito/eggolib/action/entity/CloseScreenAction.java
@@ -3,9 +3,11 @@ package io.github.eggohito.eggolib.action.entity;
 import io.github.apace100.apoli.power.factory.action.ActionFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.eggohito.eggolib.Eggolib;
+import io.github.eggohito.eggolib.mixin.ClientPlayerEntityAccessor;
 import io.github.eggohito.eggolib.networking.EggolibPackets;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -13,8 +15,22 @@ import net.minecraft.server.network.ServerPlayerEntity;
 public class CloseScreenAction {
 
     public static void action(SerializableData.Instance data, Entity entity) {
-        if (!(entity instanceof PlayerEntity)) return;
-        ServerPlayNetworking.send((ServerPlayerEntity) entity, EggolibPackets.CLOSE_SCREEN_CLIENT, PacketByteBufs.empty());
+
+        if (!(entity instanceof PlayerEntity playerEntity)) return;
+
+        if (playerEntity.world.isClient) {
+            MinecraftClient minecraftClient = ((ClientPlayerEntityAccessor) playerEntity).getClient();
+            minecraftClient.execute(
+                () -> minecraftClient.setScreen(null)
+            );
+        }
+
+        else ServerPlayNetworking.send(
+            (ServerPlayerEntity) playerEntity,
+            EggolibPackets.CLOSE_SCREEN_CLIENT,
+            PacketByteBufs.empty()
+        );
+
     }
 
     public static ActionFactory<Entity> getFactory() {
--- src/main/java/io/github/eggohito/eggolib/action/entity/SetPerspectiveAction.java
+++ src/main/java/io/github/eggohito/eggolib/action/entity/SetPerspectiveAction.java
@@ -4,11 +4,15 @@ import io.github.apace100.apoli.power.factory.action.ActionFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.eggohito.eggolib.Eggolib;
 import io.github.eggohito.eggolib.data.EggolibDataTypes;
+import io.github.eggohito.eggolib.mixin.ClientPlayerEntityAccessor;
 import io.github.eggohito.eggolib.networking.EggolibPackets;
 import io.github.eggohito.eggolib.util.EggolibPerspective;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.Perspective;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 
@@ -16,14 +20,47 @@ public class SetPerspectiveAction {
 
     public static void action(SerializableData.Instance data, Entity entity) {
 
-        if (!(entity instanceof ServerPlayerEntity serverPlayerEntity)) return;
+        if (!(entity instanceof PlayerEntity playerEntity)) return;
 
         EggolibPerspective eggolibPerspective = data.get("perspective");
-        PacketByteBuf buffer = new PacketByteBuf(Unpooled.buffer());
 
-        buffer.writeString(eggolibPerspective.toString());
+        if (playerEntity.world.isClient) {
 
-        ServerPlayNetworking.send(serverPlayerEntity, EggolibPackets.CHANGE_PERSPECTIVE_CLIENT, buffer);
+            MinecraftClient minecraftClient = ((ClientPlayerEntityAccessor) playerEntity).getClient();
+            minecraftClient.execute(
+                () -> {
+
+                    if (minecraftClient.player == null) return;
+
+                    switch (eggolibPerspective) {
+                        case FIRST_PERSON:
+                            minecraftClient.options.setPerspective(Perspective.FIRST_PERSON);
+                            break;
+                        case THIRD_PERSON_BACK:
+                            minecraftClient.options.setPerspective(Perspective.THIRD_PERSON_BACK);
+                            break;
+                        case THIRD_PERSON_FRONT:
+                            minecraftClient.options.setPerspective(Perspective.THIRD_PERSON_FRONT);
+                            break;
+                    }
+
+                }
+            );
+
+        }
+
+        else {
+
+            PacketByteBuf buffer = new PacketByteBuf(Unpooled.buffer());
+            buffer.writeString(eggolibPerspective.toString());
+
+            ServerPlayNetworking.send(
+                (ServerPlayerEntity) playerEntity,
+                EggolibPackets.CHANGE_PERSPECTIVE_CLIENT,
+                buffer
+            );
+
+        }
 
     }
 
