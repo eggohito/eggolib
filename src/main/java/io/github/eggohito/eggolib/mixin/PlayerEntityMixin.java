d9ee647ca0e72e98345bdd294b941414098727dc
eggohito <66972816+eggohito@users.noreply.github.com>
2022-07-12 20:08:49 +0800

    Fix issue with the bug fix for damage-modifying power types ...where the players are still damaged even though the damage condition for the power is not fulfilled

--- src/main/java/io/github/eggohito/eggolib/mixin/PlayerEntityMixin.java
+++ src/main/java/io/github/eggohito/eggolib/mixin/PlayerEntityMixin.java
@@ -37,12 +37,29 @@ public abstract class PlayerEntityMixin extends LivingEntity implements Nameable
     private void eggolib$allowDamageIfModifyingPowersExist(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
 
         boolean hasModifyingPower = false;
+
         if (source.getAttacker() != null) {
-            if (!source.isProjectile()) hasModifyingPower = PowerHolderComponent.getPowers(source.getAttacker(), ModifyDamageDealtPower.class).size() > 0;
-            else hasModifyingPower = PowerHolderComponent.getPowers(source.getAttacker(), ModifyProjectileDamagePower.class).size() > 0;
+            if (!source.isProjectile()) hasModifyingPower = PowerHolderComponent
+                .hasPower(
+                    source.getAttacker(),
+                    ModifyDamageDealtPower.class,
+                    mddp -> mddp.doesApply(source, amount, this)
+                );
+            else hasModifyingPower = PowerHolderComponent
+                .hasPower(
+                    source.getAttacker(),
+                    ModifyProjectileDamagePower.class,
+                    mpdp -> mpdp.doesApply(source, amount, this)
+                );
         }
 
-        hasModifyingPower |= PowerHolderComponent.getPowers(this, ModifyDamageTakenPower.class).size() > 0;
+        hasModifyingPower |= PowerHolderComponent
+            .hasPower(
+                this,
+                ModifyDamageTakenPower.class,
+                mdtp -> mdtp.doesApply(source, amount)
+            );
+
         if (hasModifyingPower) cir.setReturnValue(super.damage(source, amount));
 
     }
