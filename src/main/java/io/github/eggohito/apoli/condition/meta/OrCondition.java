b49140dff2a7774b64b5da992283a3e7c96358c7
eggohito <66972816+eggohito@users.noreply.github.com>
2022-11-07 17:08:26 +0800

    Add Apoli's `and`, `constant` and `or` meta condition types Since Apoli does not have those as their own classes yet, apparently. Nothing major, just to make stuff look neat

---
+++ src/main/java/io/github/eggohito/apoli/condition/meta/OrCondition.java
@@ -0,0 +1,26 @@
+package io.github.eggohito.apoli.condition.meta;
+
+import io.github.apace100.apoli.Apoli;
+import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
+import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataType;
+
+import java.util.List;
+
+public class OrCondition {
+
+    public static <T> boolean condition(SerializableData.Instance data, T t) {
+        List<ConditionFactory<T>.Instance> conditions = data.get("conditions");
+        return conditions.stream().anyMatch(condition -> condition.test(t));
+    }
+
+    public static <T> ConditionFactory<T> getFactory(SerializableDataType<ConditionFactory<T>.Instance> dataType) {
+        return new ConditionFactory<>(
+            Apoli.identifier("or"),
+            new SerializableData()
+                .add("conditions", SerializableDataType.list(dataType)),
+            OrCondition::condition
+        );
+    }
+
+}
