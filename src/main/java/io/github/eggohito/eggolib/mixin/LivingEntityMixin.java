c062cc9279d74cb4f1bbc1b365ba115008304134
eggohito <66972816+eggohito@users.noreply.github.com>
2023-02-26 19:34:27 +0800

    Tweak the impl. of the `modify_hurt_ticks` power type + Added 'immunity modifier(s)' for modifying when an entity can take damage only if the damage is greater than the last damage taken * Changed how 'hurt ticks' is modified

--- src/main/java/io/github/eggohito/eggolib/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/eggohito/eggolib/mixin/LivingEntityMixin.java
@@ -1,6 +1,8 @@
 package io.github.eggohito.eggolib.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.apace100.apoli.component.PowerHolderComponent;
+import io.github.apace100.apoli.util.modifier.ModifierUtil;
 import io.github.eggohito.eggolib.power.ModifyHurtTicksPower;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -9,25 +11,43 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = LivingEntity.class)
+import java.util.List;
+
+@Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
 
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
-    @Inject(method = "damage", at = @At(value = "TAIL"))
-    private void eggolib$modifyHurtTicks(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if (!cir.getReturnValue()) return;
-        PowerHolderComponent.withPowers(
-            (LivingEntity) (Object) this,
-            ModifyHurtTicksPower.class,
-            mhtp -> mhtp.doesApply(source, amount, source.getAttacker()),
-            mhtp -> mhtp.apply(source.getAttacker())
-        );
+    @ModifyExpressionValue(method = "damage", at = @At(value = "CONSTANT", args = "floatValue=10.0f", ordinal = 0))
+    private float eggolib$modifyImmunityTicks(float original, DamageSource source, float amount) {
+
+        LivingEntity thisAsLiving = (LivingEntity) (Object) this;
+        List<ModifyHurtTicksPower> mhtps = PowerHolderComponent.getPowers(thisAsLiving, ModifyHurtTicksPower.class)
+            .stream()
+            .filter(mhtp -> mhtp.doesApply(source, amount, source.getAttacker()))
+            .toList();
+
+        if (mhtps.isEmpty()) return original;
+        return (float) ModifierUtil.applyModifiers(thisAsLiving, mhtps.stream().flatMap(mhtp -> mhtp.getImmunityModifiers().stream()).toList(), original);
+
+    }
+
+    @ModifyExpressionValue(method = "damage", at = @At(value = "CONSTANT", args = "intValue=20"))
+    private int eggolib$modifyHurtTicks(int original, DamageSource source, float amount) {
+
+        LivingEntity thisAsLiving = (LivingEntity) (Object) this;
+        List<ModifyHurtTicksPower> mhtps = PowerHolderComponent.getPowers(thisAsLiving, ModifyHurtTicksPower.class)
+            .stream()
+            .filter(mhtp -> mhtp.doesApply(source, amount, source.getAttacker()))
+            .toList();
+
+        if (mhtps.isEmpty()) return original;
+        mhtps.forEach(mhtp -> mhtp.executeActions(source.getAttacker()));
+        return (int) ModifierUtil.applyModifiers(thisAsLiving, mhtps.stream().flatMap(mhtp -> mhtp.getModifiers().stream()).toList(), original);
+
     }
 
 }
--- src/main/java/io/github/eggohito/eggolib/power/ModifyHurtTicksPower.java
+++ src/main/java/io/github/eggohito/eggolib/power/ModifyHurtTicksPower.java
@@ -1,50 +1,65 @@
 package io.github.eggohito.eggolib.power;
 
-import io.github.apace100.apoli.component.PowerHolderComponent;
 import io.github.apace100.apoli.data.ApoliDataTypes;
+import io.github.apace100.apoli.power.Power;
 import io.github.apace100.apoli.power.PowerType;
-import io.github.apace100.apoli.power.ValueModifyingPower;
 import io.github.apace100.apoli.power.factory.PowerFactory;
 import io.github.apace100.apoli.util.modifier.Modifier;
 import io.github.apace100.calio.data.SerializableData;
-import io.github.apace100.calio.data.SerializableDataTypes;
 import io.github.eggohito.eggolib.Eggolib;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.util.Pair;
-import net.minecraft.util.math.MathHelper;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-public class ModifyHurtTicksPower extends ValueModifyingPower {
+public class ModifyHurtTicksPower extends Power {
 
     private final Consumer<Pair<Entity, Entity>> biEntityAction;
 
     private final Predicate<Pair<Entity, Entity>> biEntityCondition;
     private final Predicate<Pair<DamageSource, Float>> damageCondition;
 
-    public ModifyHurtTicksPower(PowerType<?> powerType, LivingEntity livingEntity, Consumer<Pair<Entity, Entity>> biEntityAction, Predicate<Pair<Entity, Entity>> biEntityCondition, Predicate<Pair<DamageSource, Float>> damageCondition) {
+    private final List<Modifier> modifiers = new LinkedList<>();
+    private final List<Modifier> immunityModifiers = new LinkedList<>();
+
+    public ModifyHurtTicksPower(PowerType<?> powerType, LivingEntity livingEntity, Consumer<Pair<Entity, Entity>> biEntityAction, Predicate<Pair<Entity, Entity>> biEntityCondition, Predicate<Pair<DamageSource, Float>> damageCondition, Modifier modifier, List<Modifier> modifiers, Modifier immunityModifier, List<Modifier> immunityModifiers) {
+
         super(powerType, livingEntity);
+
         this.biEntityAction = biEntityAction;
         this.biEntityCondition = biEntityCondition;
         this.damageCondition = damageCondition;
+
+        if (modifier != null) this.modifiers.add(modifier);
+        if (modifiers != null) this.modifiers.addAll(modifiers);
+
+        if (immunityModifier != null) this.immunityModifiers.add(immunityModifier);
+        if (immunityModifiers != null) this.immunityModifiers.addAll(immunityModifiers);
+
+    }
+
+    public List<Modifier> getModifiers() {
+        return modifiers;
+    }
+
+    public List<Modifier> getImmunityModifiers() {
+        return immunityModifiers;
     }
 
     public boolean doesApply(DamageSource damageSource, float damageAmount, Entity attacker) {
         return (damageCondition == null || damageCondition.test(new Pair<>(damageSource, damageAmount))) && (biEntityCondition == null || biEntityCondition.test(new Pair<>(attacker, entity)));
     }
 
-    public void apply(Entity attacker) {
-        entity.timeUntilRegen = MathHelper.clamp((int) PowerHolderComponent.modify(entity, this.getClass(), entity.timeUntilRegen), 0, Integer.MAX_VALUE);
+    public void executeActions(Entity attacker) {
         if (biEntityAction != null) biEntityAction.accept(new Pair<>(attacker, entity));
     }
 
     public static PowerFactory<?> getFactory() {
-
         return new PowerFactory<>(
             Eggolib.identifier("modify_hurt_ticks"),
             new SerializableData()
@@ -52,25 +67,21 @@ public class ModifyHurtTicksPower extends ValueModifyingPower {
                 .add("bientity_condition", ApoliDataTypes.BIENTITY_CONDITION, null)
                 .add("damage_condition", ApoliDataTypes.DAMAGE_CONDITION, null)
                 .add("modifier", Modifier.DATA_TYPE, null)
-                .add("modifiers", Modifier.LIST_TYPE, null),
-            data -> (powerType, livingEntity) -> {
-
-                ModifyHurtTicksPower mhtp = new ModifyHurtTicksPower(
-                    powerType,
-                    livingEntity,
-                    data.get("bientity_action"),
-                    data.get("bientity_condition"),
-                    data.get("damage_condition")
-                );
-
-                data.ifPresent("modifier", mhtp::addModifier);
-                data.<List<Modifier>>ifPresent("modifiers", modifiers -> modifiers.forEach(mhtp::addModifier));
-
-                return mhtp;
-
-            }
+                .add("modifiers", Modifier.LIST_TYPE, null)
+                .add("immunity_modifier", Modifier.DATA_TYPE, null)
+                .add("immunity_modifiers", Modifier.LIST_TYPE, null),
+            data -> (powerType, livingEntity) -> new ModifyHurtTicksPower(
+                powerType,
+                livingEntity,
+                data.get("bientity_action"),
+                data.get("bientity_condition"),
+                data.get("damage_condition"),
+                data.get("modifier"),
+                data.get("modifiers"),
+                data.get("immunity_modifier"),
+                data.get("immunity_modifiers")
+            )
         ).allowCondition();
-
     }
 
 }
