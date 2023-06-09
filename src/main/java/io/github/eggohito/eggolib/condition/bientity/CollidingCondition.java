542ab76baac253eebe55b04e95ce1430023c1b45
eggohito <66972816+eggohito@users.noreply.github.com>
2023-06-09 20:49:29 +0800

    Add `colliding` bi-entity condition type

---
+++ src/main/java/io/github/eggohito/eggolib/condition/bientity/CollidingCondition.java
@@ -0,0 +1,42 @@
+package io.github.eggohito.eggolib.condition.bientity;
+
+import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
+import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataTypes;
+import io.github.eggohito.eggolib.Eggolib;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.Pair;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+
+public class CollidingCondition {
+
+	public static boolean condition(SerializableData.Instance data, Pair<Entity, Entity> actorAndTarget) {
+
+		Box actorBoundingBox = actorAndTarget.getLeft().getBoundingBox();
+		Box targetBoundingBox = actorAndTarget.getRight().getBoundingBox();
+		Vec3d offset = data.get("offset");
+
+		Box offsetActorBoundingBox = actorBoundingBox;
+		if (offset != null) {
+			offsetActorBoundingBox = actorBoundingBox.offset(
+				offset.getX() * actorBoundingBox.getXLength(),
+				offset.getY() * actorBoundingBox.getYLength(),
+				offset.getZ() * actorBoundingBox.getZLength()
+			);
+		}
+
+		return offsetActorBoundingBox.intersects(targetBoundingBox);
+
+	}
+
+	public static ConditionFactory<Pair<Entity, Entity>> getFactory() {
+		return new ConditionFactory<>(
+			Eggolib.identifier("colliding"),
+			new SerializableData()
+				.add("offset", SerializableDataTypes.VECTOR, null),
+			CollidingCondition::condition
+		);
+	}
+
+}
--- src/main/java/io/github/eggohito/eggolib/registry/factory/EggolibBiEntityConditions.java
+++ src/main/java/io/github/eggohito/eggolib/registry/factory/EggolibBiEntityConditions.java
@@ -2,9 +2,7 @@ package io.github.eggohito.eggolib.registry.factory;
 
 import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
 import io.github.apace100.apoli.registry.ApoliRegistries;
-import io.github.eggohito.eggolib.condition.bientity.CompareScoreCondition;
-import io.github.eggohito.eggolib.condition.bientity.EqualCondition;
-import io.github.eggohito.eggolib.condition.bientity.HasMatchingTagCondition;
+import io.github.eggohito.eggolib.condition.bientity.*;
 import io.github.eggohito.eggolib.condition.meta.ChanceCondition;
 import net.minecraft.entity.Entity;
 import net.minecraft.registry.Registry;
@@ -14,6 +12,7 @@ public class EggolibBiEntityConditions {
 
 	public static void register() {
 		register(ChanceCondition.getFactory());
+		register(CollidingCondition.getFactory());
 		register(CompareScoreCondition.getFactory());
 		register(EqualCondition.getFactory());
 		register(HasMatchingTagCondition.getFactory());
