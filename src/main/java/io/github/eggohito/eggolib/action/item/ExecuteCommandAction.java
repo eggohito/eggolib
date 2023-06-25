f9e0dd500f534127aa996fb3b9679c60200bfb3a
eggohito <66972816+eggohito@users.noreply.github.com>
2023-06-25 17:58:58 +0800

    Remove the storage part of the `execute_command` item action type

--- src/main/java/io/github/eggohito/eggolib/action/item/ExecuteCommandAction.java
+++ src/main/java/io/github/eggohito/eggolib/action/item/ExecuteCommandAction.java
@@ -5,17 +5,13 @@ import io.github.apace100.apoli.power.factory.action.ActionFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import io.github.eggohito.eggolib.Eggolib;
-import net.minecraft.command.DataCommandStorage;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.registry.Registries;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandOutput;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
 import net.minecraft.util.math.Vec2f;
 import net.minecraft.world.World;
@@ -46,27 +42,6 @@ public class ExecuteCommandAction {
 		);
 
 		String command = data.get("command");
-		Identifier storageId = data.get("storage_id");
-		DataCommandStorage dataCommandStorage = server.getDataCommandStorage();
-
-		if (storageId != null) {
-
-			NbtCompound stackNbt = new NbtCompound();
-			NbtCompound itemNbt = new NbtCompound();
-
-			stackNbt.putString("id", Registries.ITEM.getId(stack.getItem()).toString());
-			stackNbt.putByte("Count", (byte) stack.getCount());
-			stackNbt.put("tag", stack.getOrCreateNbt());
-
-			itemNbt.put("Item", stackNbt);
-
-			NbtCompound storageNbt = dataCommandStorage.get(storageId);
-			storageNbt.copyFrom(itemNbt);
-
-			dataCommandStorage.set(storageId, storageNbt);
-
-		}
-
 		server.getCommandManager().executeWithPrefix(source, command);
 
 	}
@@ -75,8 +50,7 @@ public class ExecuteCommandAction {
 		return new ActionFactory<>(
 			Eggolib.identifier("execute_command"),
 			new SerializableData()
-				.add("command", SerializableDataTypes.STRING)
-				.add("storage_id", SerializableDataTypes.IDENTIFIER, null),
+				.add("command", SerializableDataTypes.STRING),
 			ExecuteCommandAction::action
 		);
 	}
